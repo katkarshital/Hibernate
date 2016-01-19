266ceab14f1ae3b4da80cd214d3dabe3b80a6b2e
katkarshital <shitalvkatkar@gmail.com>
2016-01-19 11:20:34 -0800

    Hibernate Code 1

--- FirstHibernateApplication/src/org/mail/hibernate/HibernateTest.java
+++ FirstHibernateApplication/src/org/mail/hibernate/HibernateTest.java
@@ -1,21 +1,19 @@
 package org.mail.hibernate;
 
 import java.util.Date;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.mail.test.UserDetails;
 
 public class HibernateTest {
-
 	public static void main(String[] args) {
 		UserDetails userObj = new UserDetails();
-		userObj.setUserId(1);
-		userObj.setUserName("User 1");
+		userObj.setUserId(11);
+		userObj.setUserName("User 11");
 		userObj.setJoingDate(new Date());
-		userObj.setAddress("User 1 Address");
-		userObj.setDescription("User 1 details");
+		userObj.setAddress("User 11 Address");
+		userObj.setDescription("User 11 details");
 		
 		SessionFactory sessionFactoryObj = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactoryObj.openSession();
